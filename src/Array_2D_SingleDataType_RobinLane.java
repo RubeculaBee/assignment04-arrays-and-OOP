fc2fca1f5207b1006f5ef666824cb282ac73c808
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-03-04 15:41:24 -0800

    Defined both 2D arrays, both normal and jagged.

--- src/Array_2D_SingleDataType_RobinLane.java
+++ src/Array_2D_SingleDataType_RobinLane.java
@@ -2,6 +2,29 @@ public class Array_2D_SingleDataType_RobinLane
 {
     public static void main(String[] args)
     {
+        char[][] alphabetNormal; //Standard array with 7 rows, each row with 7 columns
+        char[][] alphabetJagged; //Jagged array with 7 rows, each row with varied columns
 
+        alphabetNormal = new char[][] //Spaces used as padding to keep the array uniform
+                {
+                        {'A', 'B', 'C', 'D', 'E', 'F', 'G'},
+                        {' ', ' ', ' ', 'H', 'I', 'J', 'K'},
+                        {' ', ' ', 'L', 'M', 'N', 'O', 'P'},
+                        {' ', ' ', ' ', ' ', 'Q', 'R', 'S'},
+                        {' ', ' ', ' ', ' ', 'T', 'U', 'V'},
+                        {' ', ' ', ' ', ' ', ' ', 'W', 'X'},
+                        {' ', ' ', ' ', ' ', ' ', 'Y', 'Z'},
+                };
+
+        alphabetJagged = new char[][] //Sub-arrays of different length makes this 2D array jagged
+                {
+                        {'A', 'B', 'C', 'D', 'E', 'F', 'G'},
+                        {'H', 'I', 'J', 'K'},
+                        {'L', 'M', 'N', 'O', 'P'},
+                        {'Q', 'R', 'S'},
+                        {'T', 'U', 'V'},
+                        {'W', 'X'},
+                        {'Y', 'Z'},
+                };
     }
 }
