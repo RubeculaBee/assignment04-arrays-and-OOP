bd8908508622ea0bc1a71c193906eaec72bb5f88
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-03-04 16:02:37 -0800

    Commented methods and added project header

--- src/Array_2D_SingleDataType_RobinLane.java
+++ src/Array_2D_SingleDataType_RobinLane.java
@@ -1,3 +1,17 @@
+/*********************************************************************************
+ *
+ * File: Array_2D_SingleDataType_RobinLane.java
+ * By: Robin Lane
+ * Date: 03-04-2025
+ *
+ * Description: Initialises two 2D arrays of characters. One array is standard,
+ *              one is jagged. Each array contains the capital english letters
+ *              from A-Z. When run the program will print the contents of both
+ *              arrays to the console with the desired spacing and formatting.
+ *
+ *********************************************************************************/
+
+
 public class Array_2D_SingleDataType_RobinLane
 {
     static char[][] alphabetNormal; //Standard array with 7 rows, each row with 7 columns
@@ -8,6 +22,7 @@ public class Array_2D_SingleDataType_RobinLane
         initArrays();
     }
 
+    // Method initializes both 2D arrays. Seperated into its own method for readability
     static void initArrays()
     {
         alphabetNormal = new char[][] //Spaces used as padding to keep the array uniform
@@ -33,6 +48,8 @@ public class Array_2D_SingleDataType_RobinLane
                 };
     }
 
+    /* Takes an array of characters and returns a string of those characters with an amount of
+       spaces between each letter equal to spaceWidth */
     static String charArrayToString(char[] charArray, int spaceWidth)
     {
         String formattedString = "";
