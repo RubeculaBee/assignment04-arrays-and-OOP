c830307b8005da626fc6d5cffa485779d397ccba
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-03-06 14:29:23 -0800

    Added program header

--- src/Array_2D_MultipleDataTypes_RobinLane.java
+++ src/Array_2D_MultipleDataTypes_RobinLane.java
@@ -1,3 +1,16 @@
+/*********************************************************************************
+ *
+ * File: Array_2D_MultipleDataTypes_RobinLane.java
+ * By: Robin Lane
+ * Date: 03-06-2025
+ *
+ * Description: Takes in a series of inputs from the user of different data types
+ *              (int, char, string), then stores it all into a 2D array.
+ *              Then prints a table of the data type of each value entered,
+ *              followed by a table of each value entered.
+ *
+ *********************************************************************************/
+
 public class Array_2D_MultipleDataTypes_RobinLane
 {
     public static void main(String[] args)
