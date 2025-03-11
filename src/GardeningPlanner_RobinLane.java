0fcb2f9ffa133f7d1a2d35bec846d4ca10fbf13b
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-03-10 20:54:47 -0700

    Added comments to each array and to displayWelcome

--- src/GardeningPlanner_RobinLane.java
+++ src/GardeningPlanner_RobinLane.java
@@ -14,17 +14,18 @@
 
 public class GardeningPlanner_RobinLane
 {
-    static int[] avgTemp = {46, 48, 49, 50, 51, 53, 54, 55, 56, 55, 51, 47};
-    static int[] avgRain = {5, 3, 3, 1, 1, 0, 0, 0, 0, 1, 3, 4};
+    static int[] avgTemp = {46, 48, 49, 50, 51, 53, 54, 55, 56, 55, 51, 47}; // The average temperature for each month
+    static int[] avgRain = {5, 3, 3, 1, 1, 0, 0, 0, 0, 1, 3, 4}; // The average rainfall for each month
 
     public static void main(String[] args)
     {
         displayWelcome();
     }
 
+    // Prints the welcome message to the screen
     static void displayWelcome()
     {
-        System.out.println("-".repeat(89));
+        System.out.println("-".repeat(89)); // prints 89 '-' (hyphens)
         System.out.println("Welcome to the CSC 215 Gardening Planner!");
         System.out.println("-".repeat(89));
     }
