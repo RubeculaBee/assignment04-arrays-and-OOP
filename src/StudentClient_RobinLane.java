8cc39ad6dae7f06da098d868271d53319fc7070d
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-03-11 15:11:47 -0700

    Added getStudentInfo method

--- src/StudentClient_RobinLane.java
+++ src/StudentClient_RobinLane.java
@@ -7,6 +7,7 @@
  *
  * @author Duc Ta
  */
+import java.util.Scanner;
 public class StudentClient_RobinLane {
 
     public static void main(String[] args) {
@@ -14,6 +15,9 @@ public class StudentClient_RobinLane {
         // Please add code to complete this program
 
         Student[] students = initStudents(3);
+
+        for(int i = 0; i < students.length; i++)
+            getStudentInfo(students[i], i+1);
     }
 
     // Initialises and returns an array of students of the specified size, each student having no data.
@@ -27,5 +31,17 @@ public class StudentClient_RobinLane {
         }
 
         return students;
+    }
+
+    // For a given student, set their name and gpa based on information given by the user
+    // The integer 'index' is used solely by the prompt so that each student is referred to by a number
+    static void getStudentInfo(Student student, int index)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("Enter a name for student #%d: ", index);
+        student.setName(input.nextLine());
+        System.out.printf("Enter a GPA for student #%d: ", index);
+        student.setGpa(input.nextDouble());
     }
 }
