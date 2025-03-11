d560c2ab51f714150d6e8c533d790dcb88dd4bc6
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-03-10 20:46:41 -0700

    Added program header

--- src/GardeningPlanner_RobinLane.java
+++ src/GardeningPlanner_RobinLane.java
@@ -1,3 +1,17 @@
+/*********************************************************************************
+ *
+ * File: GardeningPlanner_RobinLane.java
+ * By: Robin Lane
+ * Date: 03-10-2025
+ *
+ * Description: Asks the user for information about what a plant needs to grow,
+ *              including its minimum and maximum temperatures and its minimum
+ *              rainfall. Then, using information about average rainfall and
+ *              temperatures over 12 months, the program tells the user the plants
+ *              average growth and height for each month.
+ *
+ *********************************************************************************/
+
 public class GardeningPlanner_RobinLane
 {
     public static void main(String[] args)
