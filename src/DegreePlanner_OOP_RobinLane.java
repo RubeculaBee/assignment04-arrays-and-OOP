6de1ac36d4748361272fd5394e1a4859be4c5381
Robin Lane <183433480+rubeculabee@users.noreply.github.com>
2025-03-16 15:52:23 -0700

    Completed first pass of PART E2 Program

--- src/DegreePlanner_OOP_RobinLane.java
+++ src/DegreePlanner_OOP_RobinLane.java
@@ -20,6 +20,27 @@ public class DegreePlanner_OOP_RobinLane
         String[] semester03 = {"csc301", "csc302", "csc303", "csc304", "csc305", "csc306"};
         String[] semester04 = {"csc401", "csc402", "csc403", "csc404", "csc405", "csc406"};
 
+        Semester[] semesters = initSemesterArray(semester01, semester02, semester03, semester04);
 
+        displaySemesterArray(semesters);
+    }
+
+    // Returns an array of Semester objects, each constructed using a given String array
+    static Semester[] initSemesterArray(String[]... stringSemesters)
+    {
+        Semester[] semesters = new Semester[stringSemesters.length];
+
+        for(int i = 0; i < stringSemesters.length; i++)
+            semesters[i] = new Semester(stringSemesters[i]);
+
+        return semesters;
+    }
+
+    // Prints an array of Semester objects
+    static void displaySemesterArray(Semester[] semesters)
+    {
+        System.out.printf("Printing data... from one 1D Semester[] array containing %d items:\n", semesters.length);
+        for(Semester semester : semesters)
+            System.out.println(semester);
     }
 }
