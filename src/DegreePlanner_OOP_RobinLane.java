7f8b3365edb87369b70edf66e75b35ba058cce62
Robin Lane <183433480+rubeculabee@users.noreply.github.com>
2025-03-16 15:09:24 -0700

    Initial commit of PART E2 program and Semester Class

---
+++ src/DegreePlanner_OOP_RobinLane.java
@@ -0,0 +1,20 @@
+/*********************************************************************************
+ *
+ * File: DegreePlanner_OOP_RobinLane.java
+ * By: Robin Lane
+ * Date: 03-16-2025
+ *
+ * Description: Defines 4 1D arrays of Strings (representing a students
+ *              classes for each semester), Then loads all of those
+ *              strings into a 1D array of Semester objects. It then prints each
+ *              string to the console.
+ *
+ *********************************************************************************/
+
+public class DegreePlanner_OOP_RobinLane
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/Semester.java
@@ -0,0 +1,15 @@
+/*********************************************************************************
+ *
+ * File: Semester.java
+ * By: Robin Lane
+ * Date: 03-16-2025
+ *
+ * Description: This class represents a semester of course that a student will
+ *              take. It stores a list of courses as an attribute, and will be able
+ *              to set and get those courses as behaviours.
+ *
+ *********************************************************************************/
+
+public class Semester
+{
+}
