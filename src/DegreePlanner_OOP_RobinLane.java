8a327ed5c13eecd9b1296880fbbe4dce42dca9fd
Robin Lane <183433480+rubeculabee@users.noreply.github.com>
2025-03-16 15:38:36 -0700

    Imported semester arrays to PART E2 program

--- src/DegreePlanner_OOP_RobinLane.java
+++ src/DegreePlanner_OOP_RobinLane.java
@@ -15,6 +15,11 @@ public class DegreePlanner_OOP_RobinLane
 {
     public static void main(String[] args)
     {
+        String[] semester01 = {"csc101", "csc102", "csc103", "csc104", "csc105", "csc106"};
+        String[] semester02 = {"csc201", "csc202", "csc203", "csc204", "csc205", "csc206"};
+        String[] semester03 = {"csc301", "csc302", "csc303", "csc304", "csc305", "csc306"};
+        String[] semester04 = {"csc401", "csc402", "csc403", "csc404", "csc405", "csc406"};
+
 
     }
 }
